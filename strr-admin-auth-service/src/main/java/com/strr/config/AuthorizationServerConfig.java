d58090d574aeb9d81b2b803bae4f795ff53cd9bd
strr0 <38580170+strr0@users.noreply.github.com>
2023-07-12 10:55:08 +0800

    feat:授权修改

--- strr-admin-auth-service/src/main/java/com/strr/config/AuthorizationServerConfig.java
+++ strr-admin-auth-service/src/main/java/com/strr/config/AuthorizationServerConfig.java
@@ -5,14 +5,12 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import com.strr.admin.model.SysUserDetails;
-import com.strr.config.security.OAuth2FailureHandler;
-import com.strr.config.security.OAuth2Scope;
-import com.strr.config.security.OAuth2SuccessHandler;
+import com.strr.config.security.ScopeWithDescription;
 import com.strr.jose.Jwks;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,6 +18,8 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -37,14 +37,8 @@ import java.util.UUID;
 
 @Configuration
 public class AuthorizationServerConfig {
-    @Value("${oauth2.web:http://localhost:8080}")
-    private String web;
-
-    @Value("${oauth2.client:STRR_CLIENT}")
-    private String client;
-
-    @Value("${oauth2.secret:STRR_SECRET}")
-    private String secret;
+    private static final String CUSTOM_CONSENT_PAGE_URI = "/oauth2/consent";
+    private static final String REDIRECT_URI = "http://localhost:8080";
 
     /**
      * A Spring Security filter chain for the Protocol Endpoints.
@@ -53,24 +47,20 @@ public class AuthorizationServerConfig {
     @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
-        // 自定义授权页面
-        authorizationServerConfigurer.authorizationEndpoint(endpoint ->
-                endpoint.consentPage(web + "/security/oauth2/consent")
-                        .authorizationResponseHandler(new OAuth2SuccessHandler())
-                        .errorResponseHandler(new OAuth2FailureHandler())
-        );
+        authorizationServerConfigurer.authorizationEndpoint(authorizationEndpoint ->
+                        authorizationEndpoint.consentPage(CUSTOM_CONSENT_PAGE_URI))
+                .oidc(Customizer.withDefaults());	// Enable OpenID Connect 1.0
+
         RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+
         http.securityMatcher(endpointsMatcher)
-                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
-                .apply(authorizationServerConfigurer)
-                .and()
-                // Redirect to the login page when not authenticated from the
-                // authorization endpoint
-                .exceptionHandling((exceptions) -> exceptions.authenticationEntryPoint(
-                        new LoginUrlAuthenticationEntryPoint("/login")))
-                // Accept access tokens for User Info and/or Client Registration
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                .exceptionHandling(exceptions ->
+                        exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .apply(authorizationServerConfigurer);
         return http.build();
     }
 
@@ -80,8 +70,8 @@ public class AuthorizationServerConfig {
     @Bean
     public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-                .clientId(client)
-                .clientSecret(passwordEncoder.encode(secret))
+                .clientId("STRR_CLIENT")
+                .clientSecret("$2a$10$OorZmxepTUuFZKzRVm1j2O6aVRDRaFq4nQ/kI5Gu4tFyPy5qqawP.")   // STRR_SECRET
                 .clientAuthenticationMethods(methods -> {
                     methods.add(ClientAuthenticationMethod.CLIENT_SECRET_BASIC);
                     methods.add(ClientAuthenticationMethod.CLIENT_SECRET_POST);
@@ -89,12 +79,8 @@ public class AuthorizationServerConfig {
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .redirectUri(web)
-                .scopes(scopes -> {
-                    for (OAuth2Scope scope : OAuth2Scope.values()) {
-                        scopes.add(scope.getCode());
-                    }
-                })
+                .redirectUri(REDIRECT_URI)
+                .scopes(ScopeWithDescription::addScope)
                 .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                 .build();
         return new InMemoryRegisteredClientRepository(registeredClient);
@@ -124,6 +110,12 @@ public class AuthorizationServerConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    public OAuth2AuthorizationConsentService authorizationConsentService() {
+        // Will be used by the ConsentController
+        return new InMemoryOAuth2AuthorizationConsentService();
     }
 
     /**
---
+++ strr-admin-auth-service/src/main/java/com/strr/web/AuthorizationConsentController.java
@@ -0,0 +1,71 @@
+package com.strr.web;
+
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.*;
+
+import com.strr.config.security.ScopeWithDescription;
+
+/**
+ * @author Daniel Garnier-Moiroux
+ */
+@Controller
+public class AuthorizationConsentController {
+	private final RegisteredClientRepository registeredClientRepository;
+	private final OAuth2AuthorizationConsentService authorizationConsentService;
+
+	public AuthorizationConsentController(RegisteredClientRepository registeredClientRepository,
+                                          OAuth2AuthorizationConsentService authorizationConsentService) {
+		this.registeredClientRepository = registeredClientRepository;
+		this.authorizationConsentService = authorizationConsentService;
+	}
+
+	@GetMapping(value = "/oauth2/consent")
+	public String consent(Principal principal, Model model,
+			@RequestParam(OAuth2ParameterNames.CLIENT_ID) String clientId,
+			@RequestParam(OAuth2ParameterNames.SCOPE) String scope,
+			@RequestParam(OAuth2ParameterNames.STATE) String state) {
+
+		// Remove scopes that were already approved
+		Set<String> scopesToApprove = new HashSet<>();
+		Set<String> previouslyApprovedScopes = new HashSet<>();
+		RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
+		OAuth2AuthorizationConsent currentAuthorizationConsent =
+				this.authorizationConsentService.findById(registeredClient.getId(), principal.getName());
+		Set<String> authorizedScopes;
+		if (currentAuthorizationConsent != null) {
+			authorizedScopes = currentAuthorizationConsent.getScopes();
+		} else {
+			authorizedScopes = Collections.emptySet();
+		}
+		for (String requestedScope : StringUtils.delimitedListToStringArray(scope, " ")) {
+			if (OidcScopes.OPENID.equals(requestedScope)) {
+				continue;
+			}
+			if (authorizedScopes.contains(requestedScope)) {
+				previouslyApprovedScopes.add(requestedScope);
+			} else {
+				scopesToApprove.add(requestedScope);
+			}
+		}
+
+		model.addAttribute("clientId", clientId);
+		model.addAttribute("state", state);
+		model.addAttribute("scopes", ScopeWithDescription.withDescription(scopesToApprove));
+		model.addAttribute("previouslyApprovedScopes", ScopeWithDescription.withDescription(previouslyApprovedScopes));
+		model.addAttribute("principalName", principal.getName());
+
+		return "consent";
+	}
+}
